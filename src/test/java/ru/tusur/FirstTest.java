f21b027f2ac4b6bd785f818a144ce0585c0973f8
Zefirka-SATANA <hl655@mail.ru>
2019-06-06 17:39:13 +0700

    Оптимизация

--- src/test/java/ru/tusur/FirstTest.java
+++ src/test/java/ru/tusur/FirstTest.java
@@ -1,22 +1,35 @@
 package ru.tusur;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
 
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Users/Admin/chromedriver.exe");
+
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void FirstTest() {
-        System.setProperty("webdriver.chrome.driver",
-                   "C:/Users/Admin/chromedriver.exe");
 
-        ChromeDriver driver = new ChromeDriver();
         driver.get("https://tusur.ru/");
 
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Томский государственный университет систем управления и радиоэлектроники"));
 
+    }
+
+    @After
+    public void close() {
         driver.quit();
     }
 }
